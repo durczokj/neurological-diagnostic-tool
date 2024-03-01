d03cad7d4690e335f66398c706cc7ea4c17ac2f0
martazielinska <rrot@5CG2145LMG-W10.int.pg.com>
2024-03-01 14:59:59 +0100

    fix relations

--- backend/src/schemas/diseasesymptoms_map.py
+++ backend/src/schemas/diseasesymptoms_map.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 from tortoise.contrib.pydantic import pydantic_model_creator
 
-from src.database.models import DiseaseSymptoms_Map
+from src.database.models import DiseaseSymptomsMap
 
 
 DiseaseSymptomsMapCreateSchema = pydantic_model_creator(
