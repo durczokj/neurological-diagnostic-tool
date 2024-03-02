f0c719544184b8da18ca3c374031bba97a7ec1b3
martazielinska <rrot@5CG2145LMG-W10.int.pg.com>
2024-03-02 18:19:18 +0100

    change order

--- backend/src/schemas/symptoms.py
+++ backend/src/schemas/symptoms.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Literal
 
 from pydantic import BaseModel
 from tortoise.contrib.pydantic import pydantic_model_creator
@@ -7,7 +7,7 @@ from src.database.models import Symptoms
 
 
 SymptomCreateSchema = pydantic_model_creator(
-    Symptoms, name="SymptomCreateSchema", exclude=["modified_at", "created_at"]
+    Symptoms, name="SymptomCreateSchema", exclude=["modified_at", "created_at","diseasesymptomsmaps"]
     )
 
 SymptomOutSchema = pydantic_model_creator(
@@ -16,6 +16,16 @@ SymptomOutSchema = pydantic_model_creator(
     ]
 )
 
-SymptomResponseSchema = pydantic_model_creator(
-    Symptoms, name="SymptomResponse", exclude_readonly=True
-)
+class SymptomResponseSchema(BaseModel):
+    name: str
+    symmetry_answer: Optional[Literal["symmetric", "assymetric"]]
+    severity_answer: Optional[Literal["variable", "persistent"]]
+    age_onset_answer: Optional[Literal["newborn", "0-10y", "11-20y", "21-30y"]]
+    progressive_answer: Optional[Literal["over_days", "over_months", "over_years"]]
+    value_answer: Optional[str]
+
+
+
+symptom1 = {
+    "name":"ptosis"
+}
