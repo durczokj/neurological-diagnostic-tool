61b5b5c02866110b49abffd41ea7e63b6ee15547
martazielinska <rrot@5CG2145LMG-W10.int.pg.com>
2024-03-02 20:01:24 +0100

    change insert

--- data/database_insert.py
+++ data/database_insert.py
@@ -2,6 +2,7 @@ import requests
 import json
 import asyncio
 import os
+import time
 
 
 
@@ -17,7 +18,7 @@ def login_or_register(session, username, password):
     try:
         response = session.post('http://localhost:5001/register', register_data)
     except:
-        raise Exception(response.json())
+        raise Exception('Not able to register a user')
 
     login_data = {
         'username': username,
@@ -26,26 +27,28 @@ def login_or_register(session, username, password):
     try:
         response = session.post('http://localhost:5001/login', login_data)
     except:
-        raise Exception(response.json())
+        raise Exception(response)
 
 
 
 async def async_function(session, endpoint, data):
 
-    response = await asyncio.get_running_loop().run_in_executor(None, session.post, f'http://localhost:5001/{endpoint}', data)
+    response = session.post(f'http://localhost:5001/{endpoint}', data)
 
     if response.status_code == 200:
         print(f"Request succeded for table {endpoint}")
 
     else:
         print(response)
-        await asyncio.sleep(2)
+        time.sleep(2)
         print(f"Request failed for table {endpoint}")
 
 
 async def push_data_to_db(directory, session):
     
-    for filename in os.listdir(os.getcwd()):
+    current_file = os.path.abspath(__file__)
+    current_directory = os.path.dirname(current_file)
+    for filename in os.listdir(current_directory):
         if filename.endswith('.json'):
             endpoint = filename.replace('.json', '')
             with open(filename, 'r') as file:
@@ -55,29 +58,27 @@ async def push_data_to_db(directory, session):
                    await async_function(session, endpoint, record)
 
 
-async def health_check_loop(session, directory):
+def health_check_loop(session, directory):
     while True:
         # Perform the health check
-        response = await asyncio.get_running_loop().run_in_executor(None, session.get, 'http://localhost:5001/')
-
-        # Check the health check status
-        if response.status_code == 200:
+        try:
+            response = session.get('http://localhost:5001/')
+            print(response)
             print("Health check status is ok.")
             break
-        else:
+        except:
             print("Health check status is not ok.")
-            await asyncio.sleep(1)
+            time.sleep(1)
 
-    await push_data_to_db(directory, session)
+    
 
 async def main():
     session = requests.Session()
     directory = ''
+    health_check_loop(session, directory)
     login_or_register(session, 'marta', 'pass')
-    await health_check_loop(session, directory)
-
-    await session.close()
-
+    await push_data_to_db(directory, session)
+    
 
 if __name__ == '__main__':
     asyncio.run(main())
