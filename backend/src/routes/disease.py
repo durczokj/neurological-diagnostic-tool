5b30f97047cf017d49e8abe32d1665052a3f9136
dybciob-pg <dybcio.b@pg.com>
2024-03-06 02:01:09 +0100

    stupid

--- backend/src/routes/disease.py
+++ backend/src/routes/disease.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
+from src.schemas.symptoms import SymptomResponseSchema
 from tortoise.exceptions import DoesNotExist
 from tortoise.contrib.fastapi import HTTPNotFoundError
+from tortoise.queryset import Q
 from typing import List
 
 from src.schemas.diseasesymptoms_map import DiseaseSymptomsMapOutSchema
@@ -110,3 +112,45 @@ async def match_diseases_with_symptoms(symptoms: List[DiseaseSymptomsMapOutSchem
     ]
 
     return result
+
+
+@router.post("/disease/from_symptoms", response_model=List[DiseaseMatchOutSchema])
+async def get_diseases_from_symptoms(symptoms: List[SymptomResponseSchema]) -> List[DiseaseMatchOutSchema]:
+    diseases_matching = []
+    disease_matching_count = {}
+    for symptom in symptoms:
+        # Build the query dynamically based on provided characteristics
+        query = Q(symptom__name=symptom.name)
+        
+        # Add conditions for each characteristic if provided
+        if symptom.symmetry_answer:
+            query &= Q(characteristic__name="symetria", characteristic__value=symptom.symmetry_answer)
+        if symptom.variability_answer:
+            query &= Q(characteristic__name="zmienność w czasie", characteristic__value=symptom.variability_answer)
+        if symptom.age_onset_answer:
+            query &= Q(characteristic__name="wiek podczas wystapienia pierwszych objawów", characteristic__value=symptom.age_onset_answer)
+        if symptom.progressive_answer:
+            query &= Q(characteristic__name="pogorszenie w ciągu", characteristic__value=symptom.progressive_answer)
+        # Add custom logic for value_answer if applicable
+
+        # Perform the query using the built conditions
+        matching_maps = await DiseaseSymptomsMap.filter(query).distinct().prefetch_related('disease')
+
+        for match in matching_maps:
+            # Update count for each disease found
+            if match.disease.name not in disease_matching_count:
+                disease_matching_count[match.disease.name] = {'disease': match.disease, 'count': 1}
+            else:
+                disease_matching_count[match.disease.name]['count'] += 1
+
+        sorted_diseases = sorted(disease_matching_count.values(), key=lambda x: x['count'], reverse=True)
+
+        # Convert sorted diseases to the new output schema, including the count
+        result = [
+            DiseaseMatchOutSchema(
+                **(await DiseaseOutSchema.from_tortoise_orm(entry['disease'])).dict(),
+                matching_symptoms_count=entry['count'])
+            for entry in sorted_diseases
+        ]
+
+        return result
