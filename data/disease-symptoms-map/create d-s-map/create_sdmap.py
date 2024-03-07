27cceb785f9b348809c5405c364a687f4a2cd895
martazielinska <rrot@5CG2145LMG-W10.int.pg.com>
2024-03-07 21:38:23 +0100

    full data

--- data/disease-symptoms-map/create d-s-map/create_sdmap.py
+++ data/disease-symptoms-map/create d-s-map/create_sdmap.py
@@ -1,12 +1,20 @@
 import json
 
-is_initial_symptom = True
+is_initial_symptom = False
 #my_symptoms = ["Dystalny niedowład kończyn górnych", "Dystalny niedowład kończyn dolnych" , 
-#                "Proksymalny niedowład kończyn górnych", "Proksymalny niedowład kończyn dolnych" ]
-             
-my_symptoms = ['Ptoza', "Dwojenie obrazu","Dysfagia", "Zaburzenia gryzienia/żucia pokarmów", "Duszność",
-                "Zaburzenia mowy", "Proksymalny niedowład kończyn górnych", "Proksymalny niedowład kończyn dolnych",
-              "Dystalny niedowład kończyn górnych", "Dystalny niedowład kończyn dolnych" ]
+#                "Proksymalny niedowład kończyn górnych", "Proksymalny niedowład kończyn dolnych",
+#                "Niedomykanie powiek", "Uśmiech poprzeczny", "Brak możliwości gwizdania/nadęcia policzków"]
+
+#my_symptoms = ["CK"]
+#my_symptoms = ["Ptoza",  "Proksymalny niedowład kończyn górnych", "Proksymalny niedowład kończyn dolnych",
+#                "Niedowład mięśni osiowych"]
+
+#my_symptoms = ["Łysienie czołowe", "Zaćma", "Insulinoodporność", "OBS (obturacyjny bezdech senny)",  "Ból mięśni" ,
+#             "Hipotonia", "Zaburzenia rytmu serca"]
+#my_symptoms = ['Ptoza', "Dysfagia", "Zaburzenia gryzienia/żucia pokarmów",
+#                "Zaburzenia mowy", "Proksymalny niedowład kończyn górnych", "Proksymalny niedowład kończyn dolnych",
+#              "Dystalny niedowład kończyn górnych", "Dystalny niedowład kończyn dolnych" 
+#              ]
 #my_symptoms = ["RZS - choroba współistniejąca","Hashimoto lub niedoczynność tarczycy - choroba współistniejąca","Choroba Graves-Basedowa - choroba współistniejąca","Cukrzyca t1 - choroba współistniejąca"
 #               "Pogorszenie po wysiłku", "Poprawa po odpoczynku","Pogorszenie w czasie infekcji"]
 
@@ -14,6 +22,15 @@ my_symptoms = ['Ptoza', "Dwojenie obrazu","Dysfagia", "Zaburzenia gryzienia/żuc
 #                "Poziom CK większy niż 1000","Pogorszenie w czasie infekcji", "Zanik mięśni", "Zaburzenia czucia"]
                # "Suchość w jamie ustnej", "Suchość spojówek","Omdlenia ortostatyczne", "Impotencja", "Zaparcia", "Arefleksja"]
 
+#my_symptoms = ["Niedosłuch", "Zaburzenia rytmu serca", "Zaburzenia funkcji poznawczych", "Padaczka",
+#                "Dominujące zajęcie mięśni twarzy", "Zanik mięśni ramion", "Hiperlordoza lędźwiowa",
+#                "Wystający brzuch", "Ból mięśni", "Spanie z otwartymi oczami" ]
+
+my_symptoms = ["Ból mięśni", "Niepłodność - oligospermia", "Zanik jąder", "Drżenie", ]
+
+#my_symptoms = ["Hiperlordoza lędźwiowa", "Powiększony język", "Hipotonia", "Przewlekła niewydolność oddechowa", "Zaparcia"
+#                "Przodopochylenie miednicy", "Przerost łydek", "Skolioza", "Ból mięśni", "Kifoza", "Kardiomiopatia",
+#                "Powoli postępujące zajęcie obręczy biodrowej i barkowej"]
 
 #my_symptoms = ["Zaburzenia czucia", "Dominujące zajęcie mięśni obręczy barkowej (ramion) i biodrowej (ud)" ]
 
@@ -25,7 +42,7 @@ my_dict = {
         "required": "false",
         "excluding": "false",
         "characteristic_id": None,
-        "disease_id": "zespół miasteniczny Lamberta-Eatona",
+        "disease_id": "SBMA (choroba Kennedy'ego)",
         "symptom_id": "zespół miasteniczny Lamberta-Eatona"
         }
 
