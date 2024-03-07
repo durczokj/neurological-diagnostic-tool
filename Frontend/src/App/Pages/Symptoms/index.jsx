1a879be1ee3ad82e20970ffdf4b349f28f993247
dybciob-pg <dybcio.b@pg.com>
2024-03-07 23:51:47 +0100

    Update symptoms frontend

--- Frontend/src/App/Pages/Symptoms/index.jsx
+++ Frontend/src/App/Pages/Symptoms/index.jsx
@@ -1,108 +1,125 @@
-import * as React from 'react';
-// import Button from '@mui/material/Button'
-// import Autocomplete from '@mui/material/Autocomplete'
-// import TextField from '@mui/material/TextField'
+// Symptoms/index.jsx
+import React, { useState, useEffect } from 'react';
 import {
-    Button,
-    Autocomplete,
-    TextField,
-    Grid,
-    Box,
-    Card,
-    CardMedia,
-    Typography
-} from '@mui/material'
-import pic from '@/App/assets/pol_pl_Masc-na-bol-dupy-w-pudeleczku-2973_4.jpg'
-
-import { useTranslation } from 'react-i18next'
-
-import { theme } from '@/shared/utils/styles'
-
-import symptomsService from '@/App/services/symptoms'
+  Button,
+  Autocomplete,
+  TextField,
+  Grid,
+  Box,
+  Card,
+  CardMedia,
+  Typography,
+  Container,
+} from '@mui/material';
+import pic from '@/App/assets/pol_pl_Masc-na-bol-dupy-w-pudeleczku-2973_4.jpg';
+import { useTranslation } from 'react-i18next';
+import { theme } from '@/shared/utils/styles';
+import symptomsService from '@/App/services/symptoms';
 
 export default function Symptoms({ renderQuestionsScreen }) {
+  const [value, setValue] = useState([]);
+  const [symptoms, setSymptoms] = useState([]);
+  const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+  const { t } = useTranslation('translations');
 
-    const [value, setValue] = React.useState([])
-    const [symptoms, setSymptoms] = React.useState([])
+  useEffect(() => {
+    symptomsService
+      .getSymptomsList()
+      .then((result) => setSymptoms(result))
+      .catch((error) => console.log(error));
+  }, []);
 
-    const { t } = useTranslation("translations")
+  const handleSymptomChange = (event, newValue) => {
+    setValue(newValue);
+    setSelectedSymptoms(newValue);
+  };
 
-    React.useEffect(() => {
-        // setSymptoms([{
-        //   name: "ból głowy",
-        //   display_name: "ból głowy",
-        //   description: "gdy boli cie glowa",
-        //   media: "nic na razie",
-        //   group: "glowa",
-        //   can_be_symmetric: false,
-        //   can_be_variable_over_time: true,
-        //   can_have_age_of_symptom_onset: true,
-        //   can_worsen_over_time: true,
-        //   can_exist_in_family: true
-        // },
-        // {
-        //   name: "ból dupy",
-        //   display_name: "ból dupy",
-        //   description: "gdy boli cie dupa",
-        //   media: "nic na razie",
-        //   group: "dupa",
-        //   can_be_symmetric: false,
-        //   can_be_variable_over_time: true,
-        //   can_have_age_of_symptom_onset: true,
-        //   can_worsen_over_time: true,
-        //   can_exist_in_family: true
-        // },
-        // {
-        //   name: "CK",
-        //   display_name: "CK",
-        //   description: "CK"
-        // }])
-        symptomsService
-            .getSymptomsList()
-            .then(result => setSymptoms(result))
-            .catch(error => console.log(error))
-        console.log(symptoms)
-    }, [])
-    
-    return (
-        <React.Fragment>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Box
-                        display="flex"
-                        height={"50vh"}
-                        alignItems="center"
-                        sx={{ justifyContent: "center", alignContent: "center" }}
-                    >
-                        <Typography variant="h6">{value.length !== 0 ? value[value.length - 1].description : t('symptoms.descriptionPlaceholder')}</Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image={pic}
-                            alt="dupa"
-                        />
-                    </Card>
-                </Grid>
-            </Grid>
-            <Autocomplete
-                multiple
-                onChange={(event, newValue) => {
-                    console.log(newValue)
-                    setValue(newValue)
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          width: '100%',
+          backgroundColor: '#0C2865',
+          color: 'white',
+          padding: '1rem',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: 1000,
+          fontFamily: 'Calibri Light',
+        }}
+      >
+        <Typography variant="h4" align="center" style={{ fontWeight: 'bold' }}>
+          Najpierw opowiedz o swoich objawach
+        </Typography>
+      </Box>
+      <Container style={{ marginTop: '6rem', fontFamily: 'Calibri Light' }}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Box mt={2} display="flex" justifyContent="flex-start" width="100%">
+              <Typography
+                variant="body1"
+                gutterBottom
+                style={{
+                  backgroundColor: '#f0f0f0',
+                  padding: '1rem',
+                  borderRadius: '1rem',
+                  maxWidth: '70%',
+                  alignSelf: 'flex-start',
                 }}
+              >
+                {t('symptoms.descriptionPlaceholder')}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Box mt={1} display="flex" justifyContent="flex-end" width="100%">
+              <Autocomplete
+                multiple
+                onChange={handleSymptomChange}
                 id="combo-box-symptoms"
                 options={symptoms}
                 getOptionLabel={(option) => option.name}
                 filterSelectedOptions
-                sx={{ width: "100%", pt: 3, pb: 3 }}
-                renderInput={(params) => <TextField {...params} label="Symptom" placeholder={t('symptoms.choose')}/>}
-            />
-            <Grid container justifyContent="flex-end">
-                <Button variant="contained" style={{maxWidth: '120px', maxHeight: '70px', minWidth: '120px', minHeight: '70px'}} onClick={() => renderQuestionsScreen(value)}>Next</Button>
+                sx={{ width: '70%', pt: 3, pb: 3 }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Symptom" placeholder={t('symptoms.choose')} />
+                )}
+              />
+            </Box>
+          </Grid>
+          {selectedSymptoms.map((symptom, index) => (
+            <Grid item xs={12} key={index}>
+              <Box mt={1} display="flex" justifyContent="flex-end" width="100%">
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  style={{
+                    backgroundColor: '#f0f0f0',
+                    padding: '0.5rem',
+                    borderRadius: '1rem',
+                    maxWidth: '70%',
+                    alignSelf: 'flex-end',
+                  }}
+                >
+                  {symptom.name}: {symptom.description}
+                </Typography>
+              </Box>
             </Grid>
-        </React.Fragment>
-    )
+          ))}
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="flex-end">
+              <Button
+                variant="contained"
+                style={{ maxWidth: '120px', maxHeight: '70px', minWidth: '120px', minHeight: '70px' }}
+                onClick={() => renderQuestionsScreen(value)}
+              >
+                Next
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 }
