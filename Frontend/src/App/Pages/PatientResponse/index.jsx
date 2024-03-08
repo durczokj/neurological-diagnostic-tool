e714677d726cc82685e209d63fffc2235f9249dc
dybciob-pg <dybcio.b@pg.com>
2024-03-08 04:39:09 +0100

    Add button at the end

--- Frontend/src/App/Pages/PatientResponse/index.jsx
+++ Frontend/src/App/Pages/PatientResponse/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { Container, Typography, Box } from '@mui/material';
+import { useLocation, Link } from 'react-router-dom';
+import { Container, Typography, Box, Button } from '@mui/material';
 
 const PatientResponse = () => {
   const location = useLocation();
@@ -38,7 +38,7 @@ const PatientResponse = () => {
               alignSelf: 'flex-start',
             }}
           >
-            Na podstawie wprowadzonych objawów, istnieje prawdopodobieństwo, że cierpisz na chorobę należącą do jednej z poniższych grup. Zalecamy skonsultowanie się z lekarzem w celu uzyskania dokładniejszej diagnozy i odpowiedniego leczenia.
+            Na podstawie wprowadzonych objawów, istnieje prawdopodobieństwo, że cierpisz na chorobę nerwowo-mięśniową należącą do jednej z poniższych grup. Zalecamy skonsultowanie się z lekarzem w celu uzyskania dokładniejszej diagnozy i odpowiedniego leczenia.
           </Typography>
           {groups.map((group, index) => (
             <Typography
@@ -54,11 +54,35 @@ const PatientResponse = () => {
                 whiteSpace: 'pre-line',
               }}
             >
-                <strong style={{ fontWeight: 'bold' }}> {`Grupa Chorób: `}</strong>{group.group}
-                <strong style={{ fontWeight: 'bold' }}>{`\nLiczba pasujących objawów: `}</strong>{group.matching_symptoms_count}
+              <strong style={{ fontWeight: 'bold' }}>{`Grupa Chorób: `}</strong>{group.group}
+              <strong style={{ fontWeight: 'bold' }}>{`\nLiczba pasujących objawów: `}</strong>{group.matching_symptoms_count}
             </Typography>
           ))}
-          
+          <Typography
+            variant="body1"
+            gutterBottom
+            style={{
+              backgroundColor: '#f0f0f0',
+              padding: '1rem',
+              borderRadius: '1rem',
+              maxWidth: '80%',
+              alignSelf: 'flex-start',
+              marginTop: '1rem',
+            }}
+          >
+            Jeśli chcesz umówić się na wizytę w naszej klinice, kliknij poniższy przycisk:
+          </Typography>
+          <Box mt={2} display="flex" justifyContent="flex-end" width="100%">
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/clinic"
+              style={{ fontFamily: 'Calibri Light' }}
+            >
+              Umów wizytę
+            </Button>
+          </Box>
         </Box>
       </Container>
     </>
